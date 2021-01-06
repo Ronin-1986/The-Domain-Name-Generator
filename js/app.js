b28d9a657278eec372b52301b6ccfac6c255d167
Ronin-1986 <andresolivares1986@gmail.com>
2021-01-06 11:03:52 -0300

    ejercicio completado dia 7 AM

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var pronoun = ['the','our',];
+  var adj = ['great', 'big' ];
+  var noun = ['jogger','racoon'];
+  var domain1 = ['.com', '.cl'];
+
+function domain (arr1, arr2, arr3){
+    let phrase = [];
+    for(i = 0 ; i < pronoun.length ; i++){
+          //phrase += pronoun[i];
+        for(j = 0 ; j < pronoun.length ; j++){
+              //phrase += pronoun[i] + adj[i];
+          for(k = 0 ; k < pronoun.length; k++){
+            for(l = 0; l < pronoun.length ; l++){
+                phrase += pronoun[i] + adj[j] + noun[k] + domain1[l] + " ";
+            }
+          }
+            
+        }
+    }
+    return console.log(phrase);
+}
+
+console.log(domain(pronoun, adj, noun));
+
